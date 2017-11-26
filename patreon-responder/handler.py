d409f0b627d591c11be4d68a4f0cb27532dabb78
Alex Ellis <alexellis2@gmail.com>
2017-11-26 17:15:38 +0000

    first commit

---
+++ patreon-responder/handler.py
@@ -0,0 +1,39 @@
+import tweepy
+import os
+import requests
+import json
+
+def handle(st):
+    req = json.loads(st)
+
+    event_type = os.getenv("Http_X_Patreon_Event")
+    print("Event: " + event_type)
+
+    if event_type == "pledges:create":
+        cents = req["data"]["attributes"]["amount_cents"]
+
+        patron_link = req["data"]["relationships"]["patron"]["links"]["related"]
+
+        r = requests.get(patron_link)
+
+        if r.status_code == 200:
+            patron = r.json()
+            twitter = patron["data"]["attributes"]["twitter"]
+            tagged = ""
+            if twitter != None and len(twitter) > 0:
+                tagged = " @" + twitter
+            tweet = patron["data"]["attributes"]["full_name"] + tagged + " just pledged " + "{0:.2f}".format(float(cents/100)) + " dollar(s) to @OpenFaaS. Thank you from #TeamServerless!"
+
+            print(tweet)
+
+            auth = tweepy.OAuthHandler(os.environ["consumer_key"], os.environ["consumer_secret"])
+            auth.set_access_token(os.environ["access_token"], os.environ["access_token_secret"])
+
+            api = tweepy.API(auth)
+
+            try:
+                print("Sending: " + tweet)
+
+                api.update_status(tweet)
+            except e:
+                print(e)
